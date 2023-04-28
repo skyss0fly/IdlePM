c1c3f259f48d026ec1f87106400b007fca6653d6
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-28 16:21:08 +1000

    Update main.php

--- src/skyss0fly/IdlePM/main.php
+++ src/skyss0fly/IdlePM/main.php
@@ -5,11 +5,22 @@ namespace skyss0fly\IdlePM;
 use pocketmine\Plugin\PluginBase;
 
 class Main extends PluginBase {
+   private commandsmode
 
   public function onLoad(): void {
   $this->getLogger()->info("Loading The Idle Of the Century")
-    $config = $this->new(Folder):$this->('config.yml')
-    
-    }
+		$this->saveDefaultConfig();
+	}
 
+  public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->loadConfig();
+    }
+  
+    private function loadConfig(): void {
+        $config = $this->getConfig();
+        $this->commandsmode = $config->get("commandsmode", []);
+      
+    }
+  
 }
