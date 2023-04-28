3c98aba524e46ecac4c79bf635efc5ea4410ad44
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-28 16:29:51 +1000

    Update main.php

--- src/skyss0fly/IdlePM/main.php
+++ src/skyss0fly/IdlePM/main.php
@@ -24,4 +24,24 @@ $this->getLogger()->info("Idle Has Loaded. use /idle for list of commands and se
       
     }
   
+ class LivingThing(Main):
+	def __init__(self):
+		self.name = name
+		self.health = health
+	        self.stamina = 100
+	def heal(self):
+	        self.health = self.health + 1
+	def tire(self):
+	        self.stamina = 100
+	def hurt(self):
+	        self.health = self.health - 1
+	
+class Player(LivingThing):
+	def __init__(self,monster)
+		self.name = input('Name?')
+		self.health = 15
+		self.stamina = 100
+	def idle()
+		print('Your List of commands are: Coming Soon')
+	
 }
